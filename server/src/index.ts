28eeb3f6472b32d10b7a9d6e761702a925881398
Max <markushitstep@gmail.com>
2026-04-29 21:53:53 +0300

    check graphql is working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Server is running 🚀");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello from GraphQL server!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at ${url}`);
