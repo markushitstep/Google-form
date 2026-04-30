db5e9a3bd6e66d5bb3f52de509b668646fe87921
Max <markushitstep@gmail.com>
2026-04-30 12:45:07 +0300

    change root settings for hire current file

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,21 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: ['node_modules/**', 'dist/**'],
+    ignores: ['node_modules/**', 'dist/**', 'client/dist/**', 'server/dist/**'],
   },
+
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+
+  ...tseslint.configs.recommended.map((config) => ({
+    ...config,
+    languageOptions: {
+      ...config.languageOptions,
+      parserOptions: {
+        ...config.languageOptions?.parserOptions,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  })),
+
   eslintConfigPrettier,
 ];
